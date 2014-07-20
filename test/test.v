f7f636b6849a956a0fb7b2cfa34edfa9d99e11a1
Kiwamu Okabe <kiwamu@debian.or.jp>
2014-07-20 13:46:48 +0900

    Update test

--- test/test.v
+++ test/test.v
@@ -1,8 +1,14 @@
 Declare ML Module "injectfalse".
 
-Theorem plus_O_n'' : forall n:nat, 0 + n = n.
+Theorem plus_O_n : forall n:nat, 0 + n = n.
 Proof.
   ij_hello.
   ij_intro.
   reflexivity.
+Qed.
+
+Theorem plus_0_r : forall n:nat, n + 0 = n.
+Proof.
+  intros n. destruct n as [| n'].
+  ij_injectfalse. ij_injectfalse.
 Qed.
